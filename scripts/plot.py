4fa6cf90c87da5d293a0018db59ff0834b6a9b8a
Rishabh Singh <rishabhptr@gmail.com>
2018-02-17 10:36:58 +0530

    test3

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+import matplotlib as mpl
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
 import matplotlib.pyplot as plt
 import os
 
